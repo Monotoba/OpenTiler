c72291cf1c9ce9a08491f1e67163dd9558bf7a84
R Morgan <Monotoba@users.noreply.github.com>
2026-09-22 08:41:58 -0700

    release: update About dialog to 1.3.3

--- opentiler/main_window.py
+++ opentiler/main_window.py
@@ -2879,7 +2879,7 @@ class MainWindow(QMainWindow):
         QMessageBox.about(
             self,
             "About OpenTiler",
-            "OpenTiler v1.3.2\n\n"
+            "OpenTiler v1.3.3\n\n"
             "A PySide6-based desktop application for scaling and tiling architectural drawings.\n\n"
             "Author: Randall Morgan\n"
             "License: BSD 2-Clause License\n"
